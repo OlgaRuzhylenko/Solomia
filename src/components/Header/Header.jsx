555ee1828e4b4aafc067f4c12971090ad0ddd0cb
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2024-10-17 11:52:29 +0300

    header 768

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,104 @@
 import mainLittleLogo from "../../images/mainLittleLogo.png";
 import { NavLink } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 import css from "./Header.module.css";
+import { useState } from "react";
+import { IoIosArrowDown } from "react-icons/io";
+import NavigationList from "../NavigationList/NavigationList";
+import SubMenuAboutUs from "../SubMenuAboutUs/SubMenuAboutUs";
+import SubMenuBrands from "../SubMenuBrands/SubMenuBrands";
+import BackDrop from "../BackDrop/BackDrop";
 
-export default function Header(params) {
+export default function Header({ onNavigate }) {
+  const isTabletOrLarger = useMediaQuery({ query: "(min-width: 768px)" });
+  const [isAboutUsOpen, setIsAboutUsOpen] = useState(false);
+  const [isBrandsOpen, setIsBrandsOpen] = useState(false);
+
+  const handleLinkClick = () => {
+    onNavigate();
+    setIsAboutUsOpen(false);
+    setIsBrandsOpen(false);
+  };
+  const handleAboutUsMenu = () => {
+    setIsAboutUsOpen((prev) => !prev);
+    setIsBrandsOpen(false);
+  };
+
+  const handleBrandsMenu = () => {
+    setIsBrandsOpen((prev) => !prev);
+    setIsAboutUsOpen(false);
+  };
+  const handleCloseMenu = () => {
+    setIsAboutUsOpen(false);
+    setIsBrandsOpen(false);
+  };
+
   return (
     <header className={css.headerSection}>
       <div className="container">
-        <NavLink to="/">
-          <img
-            src={mainLittleLogo}
-            alt="логотип Соломія"
-            className={css.headerLogo}
-          />
-        </NavLink>
+        <div className={css.headerSectionBox}>
+          <NavLink to="/">
+            <img
+              src={mainLittleLogo}
+              alt="логотип Соломія"
+              className={css.headerLogo}
+            />
+          </NavLink>
+          {/* починаючи із планшетної версії */}
+          {isTabletOrLarger && (
+            <div className={css.navigationBox}>
+              <nav className={css.navigation}>
+                <div className={css.navWrap}>
+                  <NavLink to="/about-us" className={css.navigationItem}>
+                    про нас
+                  </NavLink>
+                  <button onClick={handleAboutUsMenu} className={css.navButton}>
+                    <IoIosArrowDown
+                      className={`${css.navigationSvg} ${
+                        isAboutUsOpen ? css.open : ""
+                      }`}
+                    />
+                  </button>
+                  {isAboutUsOpen && (
+                    <SubMenuAboutUs onNavigate={handleLinkClick} />
+                  )}
+                </div>
+
+                <div className={css.navWrap}>
+                  <NavLink to="/brands" className={css.navigationItem}>
+                    бренди
+                  </NavLink>
+                  <button onClick={handleBrandsMenu} className={css.navButton}>
+                    <IoIosArrowDown
+                      className={`${css.navigationSvg} ${
+                        isBrandsOpen ? css.open : ""
+                      }`}
+                    />
+                  </button>
+                  {isBrandsOpen && (
+                    <SubMenuBrands onNavigate={handleLinkClick} />
+                  )}
+                </div>
+
+                <NavLink to="/our-achievements" className={css.navigationItem}>
+                  наші досягнення
+                </NavLink>
+                <NavLink to="/useful-to-know" className={css.navigationItem}>
+                  корисно знати
+                </NavLink>
+                <NavLink to="/blog" className={css.navigationItem}>
+                  новини
+                </NavLink>
+                <NavLink to="/contacts" className={css.navigationItem}>
+                  контакти
+                </NavLink>
+              </nav>
+              {(isAboutUsOpen || isBrandsOpen) && (
+                <BackDrop handleMenu={handleCloseMenu} />
+              )}
+            </div>
+          )}
+        </div>
       </div>
     </header>
   );
