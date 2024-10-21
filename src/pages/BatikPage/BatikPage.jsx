66b74f0a166eb179b269a548bbb2a6be03af5c11
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2024-10-21 08:38:01 +0300

    1

--- src/pages/BatikPage/BatikPage.jsx
+++ src/pages/BatikPage/BatikPage.jsx
@@ -17,40 +17,43 @@ export default function BatikPage() {
               Чай «Batik» – один з лідерів чайного ринку України в масовому
               сегменті.
             </h1>
-            <img
+            {/* <img
               src={batikLogo}
               alt="логотип Батік та напис this is my tea"
-            ></img>
+            ></img> */}
             <article>
-              <p>
+              <p className={css.batikText}>
                 Чай «Batik» – один з лідерів чайного ринку України в масовому
                 сегменті. В Україні чай «Batik» з’явився у 1993 році. Це був
                 перший справжній цейлонський чай, який спробували українські
-                споживачі.
+                споживачі. «Batik» – це чай з найкращих плантацій світу. Це
+                завжди найвища якість, це яскравий характер і витончений аромат.
+                Це втілення життєвої мудрості та справжніх незмінних цінностей.
               </p>
-              <p>
-                «Batik» – це чай з найкращих плантацій світу. Це завжди найвища
-                якість, це яскравий характер і витончений аромат. Це втілення
-                життєвої мудрості та справжніх незмінних цінностей.
-              </p>
-              <p>«Batik» – це мій чай!</p>
+              <p className={css.batikSlogan}>«Batik» – це мій чай!</p>
             </article>
           </div>
         </div>
         <div className={css.batikCollectionSection}>
-          <img src={batikCollectionImg} alt="Batik Collection" />
-          <h2>Batik Collection</h2>
-          <p>
-            У Batik Collection зібрані як класичні стандарти добірних
-            цейлонських чаїв для поціновувачів міцності та терпкості, так і
-            вишукані купажовані композиції з різних чайних плантацій світу та
-            яскраві ягідно-фруктові з вибуховими смаками. Знайдіть час на
-            чаювання з «Batik»! Переведіть подих, вгамуйте напругу та відчуйте
-            себе!
-          </p>
-          <NavLink to="/batik-classic-collection" className={css.batikBtn}>
-            Ознайомитись
-          </NavLink>
+          <img
+            src={batikCollectionImg}
+            alt="Batik Collection"
+            className={css.batikCollectionImg}
+          />
+          <div className="container">
+            <h2 className={css.batikCollectionTitle}>Batik Collection</h2>
+            <p className={css.batikCollectionText}>
+              У Batik Collection зібрані як класичні стандарти добірних
+              цейлонських чаїв для поціновувачів міцності та терпкості, так і
+              вишукані купажовані композиції з різних чайних плантацій світу та
+              яскраві ягідно-фруктові з вибуховими смаками. Знайдіть час на
+              чаювання з «Batik»! Переведіть подих, вгамуйте напругу та відчуйте
+              себе!
+            </p>
+            <NavLink to="/batik-classic-collection" className={css.batikBtn}>
+              Ознайомитись
+            </NavLink>
+          </div>
         </div>
         <div className={css.batikGoldSection}>
           <img src={batikGoldImg} alt="Batik Gold Collection" />
