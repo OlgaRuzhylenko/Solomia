fe59b21c0edc3b85504b5a02792bd33cb36197ca
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2024-11-18 19:38:07 +0200

    секція корисно знати

--- src/pages/UsefulToKnow/FactsAboutCoffee/FactsAboutCoffee.jsx
+++ src/pages/UsefulToKnow/FactsAboutCoffee/FactsAboutCoffee.jsx
@@ -1,2 +1,10 @@
+import Header from "../../../components/Header/Header";
 import css from "./FactsAboutCoffee.module.css";
-export default function FactsAboutCoffee() {}
+
+export default function FactsAboutCoffee() {
+  return (
+    <div>
+      <div>{<Header />}</div>
+    </div>
+  );
+}
