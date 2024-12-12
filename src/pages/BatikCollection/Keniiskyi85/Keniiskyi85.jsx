6c61772adca1cde85d4477edd2ba4a88982c5591
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2024-12-12 09:27:05 +0200

    королівський стандарт 100 1440

--- src/pages/BatikCollection/Keniiskyi85/Keniiskyi85.jsx
+++ src/pages/BatikCollection/Keniiskyi85/Keniiskyi85.jsx
@@ -1,3 +1,65 @@
+import Footer from "../../../components/Footer/Footer";
+import Header from "../../../components/Header/Header";
+import keniiskyi85 from "../../../images/batik/classic/19.png";
 import css from "./Keniiskyi85.module.css";
 
-export default function Keniiskyi85(params) {}
+export default function Keniiskyi85() {
+  return (
+    <div>
+      <div>{<Header />}</div>
+      <section className={css.keniiskyi85Section}>
+        <div className="container">
+          <div className={css.keniiskyi85Box}>
+            <img
+              src={keniiskyi85}
+              alt="пачка чаю Батік Кенійський чай 85 грам в картонній упаковці"
+              className={css.keniiskyi85Img}
+            />
+            <div className={css.keniiskyi85Wrapper}>
+              <h1 className={css.keniiskyi85Title}>
+                Batik <br />
+                <span>КЕНІЙСЬКИЙ ЧАЙ</span>
+              </h1>
+              <h2 className={css.keniiskyi85Subtitle}>
+                Чай чорний кенійський крупнолистовий
+              </h2>
+              <p className={css.keniiskyi85Text}>
+                Вирощений високо в горах під промінням яскравого кенійського
+                сонця цей чай зачарує вас дивовижним смаком з квітково –
+                цитрусовими відтінками та горіхово – шоколадним післясмаком.
+                Влаштуйте собі справжню чайну подорож особливо насиченим та
+                глибоким смаком.
+              </p>
+              <ul className={css.keniiskyi85List}>
+                <li className={css.keniiskyi85Item}>
+                  <p className={css.keniiskyi85ItemText}>
+                    Різновид чаю: чорний чай
+                  </p>
+                </li>
+                <li className={css.keniiskyi85Item}>
+                  <p className={css.keniiskyi85ItemText}>Маса нетто: 85 г</p>
+                </li>
+                <li className={css.keniiskyi85Item}>
+                  <p className={css.keniiskyi85ItemText}>
+                    Країна походження: Кенія
+                  </p>
+                </li>
+                <li className={css.keniiskyi85Item}>
+                  <p className={css.keniiskyi85ItemText}>
+                    Форма випуску: розсипний
+                  </p>
+                </li>
+                <li className={css.keniiskyi85Item}>
+                  <p className={css.keniiskyi85ItemText}>
+                    Тип упаковки: картонна
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div>{<Footer />}</div>
+    </div>
+  );
+}
--- src/pages/BatikCollection/KorolivskyiStandart100TB/KorolivskyiStandart100TB.jsx
+++ src/pages/BatikCollection/KorolivskyiStandart100TB/KorolivskyiStandart100TB.jsx
@@ -1,3 +1,78 @@
+import Footer from "../../../components/Footer/Footer";
+import Header from "../../../components/Header/Header";
+import korolivskyiStandart100TBImg from "../../../images/batik/classic/20.png";
 import css from "./KorolivskyiStandart100TB.module.css";
 
-export default function KorolivskyiStandart100TB(params) {}
+export default function KorolivskyiStandart100TB() {
+  return (
+    <div>
+      <div>{<Header />}</div>
+      <section className={css.korolivskyiStandart100TBSection}>
+        <div className="container">
+          <div className={css.korolivskyiStandart100TBBox}>
+            <img
+              src={korolivskyiStandart100TBImg}
+              alt="пачка чаю Батік Королівський Стандарт 100 пакетиків"
+              className={css.korolivskyiStandart100TBImg}
+            />
+            <div className={css.korolivskyiStandart100TBWrapper}>
+              <h1 className={css.korolivskyiStandart100TBTitle}>
+                Batik <br />
+                <span>КОРОЛІВСЬКИЙ СТАНДАРТ</span>
+              </h1>
+              <h2 className={css.korolivskyiStandart100TBSubtitle}>
+                Чай чорний цейлонський дрібний,
+                <br />
+                100 пакетиків
+              </h2>
+              <p className={css.korolivskyiStandart100TBText}>
+                У стародавні роки чайні майстри Цейлону із покоління в покоління
+                передавали секрет чаю, що призначався лише королівській
+                династії. Лише у XX сторіччі була розгадана таємниця
+                королівського стандарту і сьогодні цей напій прикрашає меню
+                сучасних чайних гурманів своїм аристократичним смаком та
+                по-королівські вишуканим ароматом.
+              </p>
+              <p className={css.korolivskyiStandart100TBText}>
+                Терпкий та насичений смак, ніжний фруктово – солодовий аромат.
+              </p>
+              <ul className={css.korolivskyiStandart100TBList}>
+                <li className={css.korolivskyiStandart100TBItem}>
+                  <p className={css.korolivskyiStandart100TBItemText}>
+                    Різновид чаю: чорний чай
+                  </p>
+                </li>
+                <li className={css.korolivskyiStandart100TBItem}>
+                  <p className={css.korolivskyiStandart100TBItemText}>
+                    Маса нетто: 200 г
+                  </p>
+                </li>
+                <li className={css.korolivskyiStandart100TBItem}>
+                  <p className={css.korolivskyiStandart100TBItemText}>
+                    Кількість пакетиків: 100 штук
+                  </p>
+                </li>
+                <li className={css.korolivskyiStandart100TBItem}>
+                  <p className={css.korolivskyiStandart100TBItemText}>
+                    Країна походження: Шрі-Ланка (о. Цейлон)
+                  </p>
+                </li>
+                <li className={css.korolivskyiStandart100TBItem}>
+                  <p className={css.korolivskyiStandart100TBItemText}>
+                    Форма випуску: пакетований з ярликом
+                  </p>
+                </li>
+                <li className={css.korolivskyiStandart100TBItem}>
+                  <p className={css.korolivskyiStandart100TBItemText}>
+                    Тип упаковки: картонна
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div>{<Footer />}</div>
+    </div>
+  );
+}
