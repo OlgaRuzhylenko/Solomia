22a0f12237a67ed2ebcb1fa0f1860b279e279af4
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2024-12-12 19:20:52 +0200

    смарагдовий острів 1440

--- src/pages/BatikGoldCollection/KrupnyiLystOPA/KrupnyiLystOPA.jsx
+++ src/pages/BatikGoldCollection/KrupnyiLystOPA/KrupnyiLystOPA.jsx
@@ -1,3 +1,69 @@
+import Footer from "../../../components/Footer/Footer";
+import Header from "../../../components/Header/Header";
+import krupnyiLystOPAImg from "../../../images/batik/batik-gold/2.png";
 import css from "./KrupnyiLystOPA.module.css";
 
-export default function KrupnyiLystOPA() {}
+export default function KrupnyiLystOPA() {
+  return (
+    <div>
+      <div>{<Header />}</div>
+      <section className={css.krupnyiLystOPASection}>
+        <div className="container">
+          <div className={css.krupnyiLystOPABox}>
+            <img
+              src={krupnyiLystOPAImg}
+              alt="пачка чаю Батік Крупний Лист OPA 100 г"
+              className={css.krupnyiLystOPAImg}
+            />
+            <div className={css.krupnyiLystOPAWrapper}>
+              <h1 className={css.krupnyiLystOPATitle}>
+                Batik GOLD <br />
+                <span>КРУПНИЙ ЛИСТ (O.P.A.)</span>
+              </h1>
+              <h2 className={css.krupnyiLystOPASubtitle}>
+                Чай чорний цейлонський крупнолистовий,
+                <br />
+                100 г
+              </h2>
+              <p className={css.krupnyiLystOPAText}>
+                Крупнолистовий чай стандарту «O.P.А.» (Orange Pekoe A.) – чай
+                найвищої якості, для створення якого використовується лише
+                велике цілісне листя з верхівок чайного куща. Тендітний, майже
+                шовковий, смак цього чаю з легким фруктовим відтінком особливо
+                цінується шанувальниками затишного вечірнього чаювання.
+              </p>
+              <ul className={css.krupnyiLystOPAList}>
+                <li className={css.krupnyiLystOPAItem}>
+                  <p className={css.krupnyiLystOPAItemText}>
+                    Різновид чаю: чорний чай
+                  </p>
+                </li>
+                <li className={css.krupnyiLystOPAItem}>
+                  <p className={css.krupnyiLystOPAItemText}>
+                    Маса нетто: 100 г
+                  </p>
+                </li>
+                <li className={css.krupnyiLystOPAItem}>
+                  <p className={css.krupnyiLystOPAItemText}>
+                    Країна походження: Шрі-Ланка (о. Цейлон)
+                  </p>
+                </li>
+                <li className={css.krupnyiLystOPAItem}>
+                  <p className={css.krupnyiLystOPAItemText}>
+                    Форма випуску: розсипний
+                  </p>
+                </li>
+                <li className={css.krupnyiLystOPAItem}>
+                  <p className={css.krupnyiLystOPAItemText}>
+                    Тип упаковки: картонна
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div>{<Footer />}</div>
+    </div>
+  );
+}
--- src/pages/BatikGoldCollection/SmarahdovyiOstriv/SmarahdovyiOstriv.jsx
+++ src/pages/BatikGoldCollection/SmarahdovyiOstriv/SmarahdovyiOstriv.jsx
@@ -1,3 +1,76 @@
+import Footer from "../../../components/Footer/Footer";
+import Header from "../../../components/Header/Header";
+import smarahdovyiOstrivImg from "../../../images/batik/batik-gold/3.png";
 import css from "./SmarahdovyiOstriv.module.css";
 
-export default function SmarahdovyiOstriv() {}
+export default function SmarahdovyiOstriv() {
+  return (
+    <div>
+      <div>{<Header />}</div>
+      <section className={css.smarahdovyiOstrivSection}>
+        <div className="container">
+          <div className={css.smarahdovyiOstrivBox}>
+            <img
+              src={smarahdovyiOstrivImg}
+              alt="пачка чаю Батік Смарагдовий Острів 25 конвертів"
+              className={css.smarahdovyiOstrivImg}
+            />
+            <div className={css.smarahdovyiOstrivWrapper}>
+              <h1 className={css.smarahdovyiOstrivTitle}>
+                Batik GOLD <br />
+                <span>СМАРАГДОВИЙ ОСТРІВ</span>
+              </h1>
+              <h2 className={css.smarahdovyiOstrivSubtitle}>
+                Чай зелений цейлонський дрібний з мʼятою та цедрою цитрусових
+                ароматизований,
+                <br />
+                25 пакетиків у конвертах
+              </h2>
+              <p className={css.smarahdovyiOstrivText}>
+                Кожний молодий листочок, що вирощується високо у горах під
+                проміннями цейлонського сонця, перетворюється у неперевершений
+                смарагдовий напій. Освіжаюча чайна композиція на основі
+                вишуканого зеленого цейлонського чаю з додаванням солодкої м’яти
+                та освіжаючих ароматів лимона і лайма – це справжній подарунок
+                вічнозеленого острова у вашій чашці.
+              </p>
+              <ul className={css.smarahdovyiOstrivList}>
+                <li className={css.smarahdovyiOstrivItem}>
+                  <p className={css.smarahdovyiOstrivItemText}>
+                    Різновид чаю: чай зелений ароматизований
+                  </p>
+                </li>
+                <li className={css.smarahdovyiOstrivItem}>
+                  <p className={css.smarahdovyiOstrivItemText}>
+                    Маса нетто: 37,5 г
+                  </p>
+                </li>
+                <li className={css.smarahdovyiOstrivItem}>
+                  <p className={css.smarahdovyiOstrivItemText}>
+                    Кількість пакетиків: 25 штук
+                  </p>
+                </li>
+                <li className={css.smarahdovyiOstrivItem}>
+                  <p className={css.smarahdovyiOstrivItemText}>
+                    Країна походження: Шрі-Ланка (о. Цейлон)
+                  </p>
+                </li>
+                <li className={css.smarahdovyiOstrivItem}>
+                  <p className={css.smarahdovyiOstrivItemText}>
+                    Форма випуску: пакетований з ярликом у конвертах
+                  </p>
+                </li>
+                <li className={css.smarahdovyiOstrivItem}>
+                  <p className={css.smarahdovyiOstrivItemText}>
+                    Тип упаковки: картонна
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div>{<Footer />}</div>
+    </div>
+  );
+}
