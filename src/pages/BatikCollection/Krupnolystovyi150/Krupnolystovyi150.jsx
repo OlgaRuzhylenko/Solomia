78aa65ae33f2c078ba0949f6f00fd4cf419615bc
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2024-12-12 08:17:54 +0200

    зелений острів 90

--- src/pages/BatikCollection/Krupnolystovyi150/Krupnolystovyi150.jsx
+++ src/pages/BatikCollection/Krupnolystovyi150/Krupnolystovyi150.jsx
@@ -1,3 +1,72 @@
+import Footer from "../../../components/Footer/Footer";
+import Header from "../../../components/Header/Header";
+import krupnolystovyi150 from "../../../images/batik/classic/13.png";
 import css from "./Krupnolystovyi150.module.css";
 
-export default function Krupnolystovyi150(params) {}
+export default function Krupnolystovyi150() {
+  return (
+    <div>
+      <div>{<Header />}</div>
+      <section className={css.krupnolystovyi150Section}>
+        <div className="container">
+          <div className={css.krupnolystovyi150Box}>
+            <img
+              src={krupnolystovyi150}
+              alt="пачка чаю Батік крупнолистовий стандарт O.P.A. 150 грам в м'якій упаковці"
+              className={css.krupnolystovyi150Img}
+            />
+            <div className={css.krupnolystovyi150Wrapper}>
+              <h1 className={css.krupnolystovyi150Title}>
+                Batik <br />
+                <span>КРУПНОЛИСТОВИЙ (О.Р.А)</span>
+              </h1>
+              <h2 className={css.krupnolystovyi150Subtitle}>
+                Чай чорний цейлонський крупнолистовий,
+                <br />
+                150г, м/у
+              </h2>
+              <p className={css.krupnolystovyi150Text}>
+                Чорний Крупнолистовий чай стандарту «O.P.А.» (Orange Pekoe A.) –
+                чай найвищої якості, для створення якого використовується лише
+                велике цілісне листя з верхівок чайного куща. Тендітний, майже
+                шовковий, смак цього чаю з легким фруктовим відтінком особливо
+                цінується шанувальниками затишного вечірнього чаювання.
+              </p>
+              <p className={css.krupnolystovyi150Text}>
+                Терпкий та насичений смак, ніжний квітково – солодовий аромат.
+              </p>
+              <ul className={css.krupnolystovyi150List}>
+                <li className={css.krupnolystovyi150Item}>
+                  <p className={css.krupnolystovyi150ItemText}>
+                    Різновид чаю: чорний чай
+                  </p>
+                </li>
+                <li className={css.krupnolystovyi150Item}>
+                  <p className={css.krupnolystovyi150ItemText}>
+                    Маса нетто: 150 г
+                  </p>
+                </li>
+                <li className={css.krupnolystovyi150Item}>
+                  <p className={css.krupnolystovyi150ItemText}>
+                    Країна походження: Шрі-Ланка (о. Цейлон)
+                  </p>
+                </li>
+                <li className={css.krupnolystovyi150Item}>
+                  <p className={css.krupnolystovyi150ItemText}>
+                    Форма випуску: розсипний
+                  </p>
+                </li>
+                <li className={css.krupnolystovyi150Item}>
+                  <p className={css.krupnolystovyi150ItemText}>
+                    Тип упаковки: м’яка упаковка
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div>{<Footer />}</div>
+    </div>
+  );
+}
--- src/pages/BatikCollection/ZelenyiOstriv90/ZelenyiOstriv90.jsx
+++ src/pages/BatikCollection/ZelenyiOstriv90/ZelenyiOstriv90.jsx
@@ -1,3 +1,72 @@
+import Footer from "../../../components/Footer/Footer";
+import Header from "../../../components/Header/Header";
+import zelenyiOstriv90 from "../../../images/batik/classic/14.png";
 import css from "./ZelenyiOstriv90.module.css";
 
-export default function ZelenyiOstriv90(params) {}
+export default function ZelenyiOstriv90() {
+  return (
+    <div>
+      <div>{<Header />}</div>
+      <section className={css.zelenyiOstriv90Section}>
+        <div className="container">
+          <div className={css.zelenyiOstriv90Box}>
+            <img
+              src={zelenyiOstriv90}
+              alt="пачка чаю Батік Зелений Острів 90 грам в картонній упаковці"
+              className={css.zelenyiOstriv90Img}
+            />
+            <div className={css.zelenyiOstriv90Wrapper}>
+              <h1 className={css.zelenyiOstriv90Title}>
+                Batik <br />
+                <span>ЗЕЛЕНИЙ ОСТРІВ</span>
+              </h1>
+              <h2 className={css.zelenyiOstriv90Subtitle}>
+                Чай зелений китайський крупнолистовий,
+                <br />
+                90 г
+              </h2>
+              <p className={css.zelenyiOstriv90Text}>
+                Променями сонця, прохолодним гірським повітрям та росою сповитий
+                кожен молодий листочок чаю, що бережно збирається в ранкову пору
+                та перетворюється у неперевершений смарагдовий напій – подарунок
+                вічно зеленої країни.
+              </p>
+              <p className={css.zelenyiOstriv90Text}>
+                Терпкий та насичений смак, з характерною гірчинкою та квітковим
+                ароматом.
+              </p>
+              <ul className={css.zelenyiOstriv90List}>
+                <li className={css.zelenyiOstriv90Item}>
+                  <p className={css.zelenyiOstriv90ItemText}>
+                    Різновид чаю: чорний чай
+                  </p>
+                </li>
+                <li className={css.zelenyiOstriv90Item}>
+                  <p className={css.zelenyiOstriv90ItemText}>
+                    Маса нетто: 150 г
+                  </p>
+                </li>
+                <li className={css.zelenyiOstriv90Item}>
+                  <p className={css.zelenyiOstriv90ItemText}>
+                    Країна походження: Шрі-Ланка (о. Цейлон)
+                  </p>
+                </li>
+                <li className={css.zelenyiOstriv90Item}>
+                  <p className={css.zelenyiOstriv90ItemText}>
+                    Форма випуску: розсипний
+                  </p>
+                </li>
+                <li className={css.zelenyiOstriv90Item}>
+                  <p className={css.zelenyiOstriv90ItemText}>
+                    Тип упаковки: м’яка упаковка
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div>{<Footer />}</div>
+    </div>
+  );
+}
