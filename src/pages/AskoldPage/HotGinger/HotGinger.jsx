f18ce7ef2213e3ae3d812ae5bc5e3159673ca5de
OlgaRuzhylenko <134257866+OlgaRuzhylenko@users.noreply.github.com>
2024-12-17 19:15:38 +0200

    Hot Ginger 1440

--- src/pages/AskoldPage/HotGinger/HotGinger.jsx
+++ src/pages/AskoldPage/HotGinger/HotGinger.jsx
@@ -1,3 +1,66 @@
+import Footer from "../../../components/Footer/Footer";
+import Header from "../../../components/Header/Header";
+import hotGingerImg from "../../../images/askold/askoldEmotion/7.png";
 import css from "./HotGinger.module.css";
 
-export default function HotGinger(params) {}
+export default function HotGinger() {
+  return (
+    <div>
+      <div>{<Header />}</div>
+      <section className={css.hotGingerSection}>
+        <div className="container">
+          <div className={css.hotGingerBox}>
+            <img
+              src={hotGingerImg}
+              alt="пачка чаю Askold Emotion Bright Night 20 конвертів"
+              className={css.hotGingerImg}
+            />
+            <div className={css.hotGingerWrapper}>
+              <h1 className={css.hotGingerTitle}>
+                Askold Emotion <br />
+                <span>HOT GINGER</span>
+              </h1>
+              <h2 className={css.hotGingerSubtitle}>
+                Суміш з імбиру, цедри лимону, ехінацеї, шипшини та чорного чаю
+                ароматизована,
+                <br />
+                20 пакетиків у конвертах
+              </h2>
+              <p className={css.hotGingerText}>
+                Імбир та лимон, ехінацея та шипшина – це справжнє джерело
+                здоров’я та вибух енергії в твоїй чашці. Магічний рецепт –
+                бадьорить влітку, зігріває взимку.
+              </p>
+              <ul className={css.hotGingerList}>
+                <li className={css.hotGingerItem}>
+                  <p className={css.hotGingerItemText}>
+                    Різновид чаю: суміш на основі імбиру ароматизована
+                  </p>
+                </li>
+                <li className={css.hotGingerItem}>
+                  <p className={css.hotGingerItemText}>Маса нетто: 36 г</p>
+                </li>
+                <li className={css.hotGingerItem}>
+                  <p className={css.hotGingerItemText}>
+                    Кількість пакетиків: 20 штук
+                  </p>
+                </li>
+                <li className={css.hotGingerItem}>
+                  <p className={css.hotGingerItemText}>
+                    Форма випуску: пакетований з ярликом у конвертах
+                  </p>
+                </li>
+                <li className={css.hotGingerItem}>
+                  <p className={css.hotGingerItemText}>
+                    Тип упаковки: картонна
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div>{<Footer />}</div>
+    </div>
+  );
+}
